9ed0486e8ffdb0c85265c28a6779899f12fb65ae
Cody B Duncan <codybstrange93@gmail.com>
2025-03-21 12:54:53 +0100

    diy server validates now

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-  "net/http"
+	"log"
+	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-  newHandler := http.NewServeMux()
-  server := http.Server{
-    Handler: newHandler,
-    Addr: ":8080",
-  }
-  server.ListenAndServe()
-  return
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+  mux.HandleFunc("POST /api/validate_chirp", handlerValidate)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
