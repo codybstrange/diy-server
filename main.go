8e0487cb17a98e062ffb8b39ade3176aacfd41da
Cody B Duncan <codybstrange93@gmail.com>
2025-03-24 12:43:56 +0100

    Update main.go to check database url

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+  _ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"sync/atomic"
+  "os"
+  "github.com/codybstrange/diy-server/internal/database"
+  "database/sql"
+  "github.com/joho/godotenv"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+  db *database.Queries
 }
 
 func main() {
-	const filepathRoot = "."
+	godotenv.Load()
+  const filepathRoot = "."
 	const port = "8080"
+
+  dbURL := os.Getenv("DB_URL")
+  if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+  db, err := sql.Open("postgres", dbURL)
+  if err != nil {
+    log.Fatal("Error in opening postgres connection")
+  }
+  dbQueries := database.New(db)
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+    db: dbQueries,
 	}
 
 	mux := http.NewServeMux()
